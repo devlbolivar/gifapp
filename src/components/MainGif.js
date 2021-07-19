ad9a50c0080f7bae3015cfb720ef983fb3200265
Luis Bolivar <luisbolivar515@gmail.com>
2021-07-18 23:52:01 -0400

    Agrega estilos

--- src/components/MainGif.js
+++ src/components/MainGif.js
@@ -3,9 +3,10 @@ import { useFetchGif } from "../hooks/useFetchGif";
 
 export const MainGif = () => {
   useFetchGif();
+  const { data: img, loading } = useFetchGif();
   return (
     <div className="main__gif">
-      <img alt="Random Img logo" />
+      <img alt="Random Img logo" src={img.url} />
     </div>
   );
 };
--- src/helpers/getGif.js
+++ src/helpers/getGif.js
@@ -1,16 +1,11 @@
-export const getGif = async () =>{
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=cYUpwPgZWImJ3k6jBeKAz0Qk7qITpoxb`;
-    
-    const resp =  await fetch(url);
-    const {data} = await resp.json();
-    console.log(data);
-    // const gifs = data.map(gif => {
-    //     console.log(gif);
-    //     return {
-    //         id: gif.id,
-    //         title: gif.title,
-    //         url: gif.images?.fixed_height_small.url
-    //     }
-    // });
-    // return gifs;
-}
+export const getGif = async () => {
+  const url = `https://api.giphy.com/v1/gifs/random?api_key=cYUpwPgZWImJ3k6jBeKAz0Qk7qITpoxb`;
+
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  return {
+    id: data.id,
+    title: data.title,
+    url: data.images?.fixed_height_small.url,
+  };
+};
